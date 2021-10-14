c33d4f18ae2f1dc5c2f2f941055fd0ca24b6f183
Marc Moten <marcmotenii@gmail.com>
2021-10-14 14:41:02 -0500

    added local storage call for saved items

--- assets/script.js
+++ assets/script.js
@@ -6,4 +6,15 @@ saveBtn.on('click', function () {
     var content = $(this).siblings('.content')
     var time = $(this).parent().attr('id');
     localStorage.setItem(time, content);
-})
+    console.log("saved");
+});
+
+$('#9am .content').val(localStorage.getItem("9am")) 
+$('#10am .content').val(localStorage.getItem("10am")) 
+$('#11am .content').val(localStorage.getItem("11am")) 
+$('#12pm .content').val(localStorage.getItem("12pm")) 
+$('#1pm .content').val(localStorage.getItem("1pm")) 
+$('#2pm .content').val(localStorage.getItem("2pm")) 
+$('#3pm .content').val(localStorage.getItem("3pm")) 
+$('#4pm .content').val(localStorage.getItem("4pm")) 
+$('#5pm .content').val(localStorage.getItem("5pm")) 

6541fdae36e71fdc560653df8a2da6fd04e0a3d3
Marc Moten <marcmotenii@gmail.com>
2021-10-14 13:42:39 -0500

    added save button functionality

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,9 @@
 var date = moment().format('dddd, MMM, Do YYYY')
 $('#currentDay').text(date)
+var saveBtn = $('.saveBtn')
+
+saveBtn.on('click', function () {
+    var content = $(this).siblings('.content')
+    var time = $(this).parent().attr('id');
+    localStorage.setItem(time, content);
+})
